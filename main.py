b9d2cf89e80bf227771f2369ec6494c9db85b3dc
Swhan123 <seungwoohan0104@gmail.com>
2022-11-01 20:52:50 +0900

    개점처리 준비

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox
+import socket
+
+root = Tk()
+root.title("EazyOrder Server") #창 이름 지정
+root.geometry("1280x720") #창 크기 지정
+root.attributes('-fullscreen',True)
+root.resizable(False, False) #해상도 변경 불가
+
+##############여기부터 함수##############
+
+def open_store():
+    btn1.pack_forget()
+    messagebox.showinfo("알림", "개점 처리되었습니다")
+
+##############여기부터 GUI##############
+btn1 = Button(root, text="개점처리", command=open_store)
+btn1.pack()
+
+
+root.mainloop()
